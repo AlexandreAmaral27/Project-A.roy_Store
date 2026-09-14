ff10fb1589e4ff16918d5cdf558b9806d3d96571
Alexandre Amaral <148690655+AlexandreAmaral27@users.noreply.github.com>
2026-09-14 05:30:31 +0100

    Prepara cookies para producao

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -173,16 +173,16 @@ router.post("/login", async (req, res) => {
         );
 
         // Guardar token em cookie
-        res.cookie(
-            "token",
-            token,
-            {
-                httpOnly: true,
-                secure: false,
-                sameSite: "lax",
-                maxAge: 7 * 24 * 60 * 60 * 1000
-            }
-        );
+    res.cookie(
+    "token",
+    token,
+    {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "lax",
+        maxAge: 7 * 24 * 60 * 60 * 1000
+    }
+);
 
         return res.json({
 
